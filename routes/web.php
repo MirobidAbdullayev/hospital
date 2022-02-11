742f6c3e650843d7f7f10f897c98a4800c571e10
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-02-11 23:16:08 +0500

    Add Doctor

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/home',[HomeController::class,'redirect']);
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/add_doctor_view',[AdminController::class,'addview']);
+
+Route::post('/upload_doctor',[AdminController::class,'upload']);
